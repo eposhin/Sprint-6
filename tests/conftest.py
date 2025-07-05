2be1127564c0218f917e00bcfddeba1b6de440dd
Evgeniy Poshin <eposhin@yandex.ru>
2025-07-05 16:29:44 +0300

    Add project

---
+++ tests/conftest.py
@@ -0,0 +1,11 @@
+import pytest
+from selenium import webdriver
+from data import TestData
+
+@pytest.fixture
+def driver():
+    driver = webdriver.Firefox()
+    driver.maximize_window()
+    driver.get(TestData.scooter_address)
+    yield driver
+    driver.quit()
